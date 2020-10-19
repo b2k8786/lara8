b8a40117cda89c4330a58d4c2ff13ec3f90cfd03
Anil Samal <b2k8786@gmail.com>
2020-10-19 10:41:43 +0530

    Products model and migrations

---
+++ app/Http/Controllers/Products.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Illuminate\Routing\Controller as BaseController;
+
+class Products extends BaseController
+{
+    function add(Request $request)
+    {
+        echo 'ji';
+    }
+}
