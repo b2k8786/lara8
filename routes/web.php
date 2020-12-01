6a876ad0b6d2a4e59b77a9a53ee8e37206a13336
b2k8786 <b2k8786@gmail.com>
2020-12-01 16:58:43 +0530

    slug

--- routes/web.php
+++ routes/web.php
@@ -21,4 +21,4 @@ Route::get('/', function () {
 
 Route::get('/live', [Stream::class, "sse"]);
 Route::get('/dash', [Stream::class, "main"]);
-Route::get('/wiki/${query}', [Main::class, "test"]);
+Route::get('/wiki/{query}', [Main::class, "test"]);
